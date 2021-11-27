a82daf75585ae42f3663a90010e6134f18c1482a
jacob-alspaw <jacob.alspaw@hcl.com>
2021-11-27 02:51:30 -0600

    Add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'airbnb'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: [
+    'react',
+    'jest'
+  ],
+  rules: {
+    'react/jsx-one-expression-per-line': 'off',
+    'comma-dangle': ['error', 'never'],
+    'react/function-component-definition': ['error', {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
+    'jest/no-disabled-tests': 'error',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'error',
+    'jest/valid-expect': 'error'
+  }
+};
