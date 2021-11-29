9f8493ae9611c7a5edbc3e39a31c188d2ca36b99
jacob-alspaw <jacob.alspaw@hcl.com>
2021-11-28 23:35:35 -0700

    Fix rendering bug that collapsed menus on navigation

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,6 @@ import './App.scss';
 const bem = defineBlock('App');
 
 const App = () => {
-  const location = useLocation();
   const [rightPanelExpanded, setRightPanelExpanded] = useState(false);
   const toggleRightPanel = () => {
     setRightPanelExpanded(!rightPanelExpanded);
@@ -60,119 +59,122 @@ const App = () => {
   return (
     <div className={bem()}>
       <HeaderContainer
-        render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-          <>
-            <Header className={bem('header')} aria-label="Jacob Alspaw">
-              <SkipToContent />
-              <HeaderMenuButton
-                aria-label="Open menu"
-                onClick={onClickSideNavExpand}
-                isActive={isSideNavExpanded}
-              />
-              <img
-                className={bem('profile-image')}
-                src="/profile-circle--small.png"
-                alt="profile"
-              />
-              <HeaderName href="#" prefix="Jacob">
-                [Alspaw]
-              </HeaderName>
-              <HeaderGlobalBar>
-                <HeaderGlobalAction aria-label="Location" onClick={() => {}}>
-                  <LocationPerson20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction aria-label="Phone" onClick={() => {}}>
-                  <Phone20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction aria-label="Email" onClick={() => {}}>
-                  <Email20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction
-                  aria-label="Applications"
-                  tooltipAlignment="end"
-                  isActive={rightPanelExpanded}
-                  onClick={toggleRightPanel}
-                >
-                  <AppSwitcher20 />
-                </HeaderGlobalAction>
-              </HeaderGlobalBar>
-              <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
-                <SideNavItems>
-                  <SideNavLink
-                    large
-                    renderIcon={UserAvatar20}
-                    href={`#${Paths.HOME}`}
-                    isActive={isPathMatch(Paths.HOME, location.pathname)}
+        render={({ isSideNavExpanded, onClickSideNavExpand }) => {
+          const location = useLocation();
+          return (
+            <>
+              <Header className={bem('header')} aria-label="Jacob Alspaw">
+                <SkipToContent />
+                <HeaderMenuButton
+                  aria-label="Open menu"
+                  onClick={onClickSideNavExpand}
+                  isActive={isSideNavExpanded}
+                />
+                <img
+                  className={bem('profile-image')}
+                  src="/profile-circle--small.png"
+                  alt="profile"
+                />
+                <HeaderName href="#" prefix="Jacob">
+                  [Alspaw]
+                </HeaderName>
+                <HeaderGlobalBar>
+                  <HeaderGlobalAction aria-label="Location" onClick={() => {}}>
+                    <LocationPerson20 />
+                  </HeaderGlobalAction>
+                  <HeaderGlobalAction aria-label="Phone" onClick={() => {}}>
+                    <Phone20 />
+                  </HeaderGlobalAction>
+                  <HeaderGlobalAction aria-label="Email" onClick={() => {}}>
+                    <Email20 />
+                  </HeaderGlobalAction>
+                  <HeaderGlobalAction
+                    aria-label="Applications"
+                    tooltipAlignment="end"
+                    isActive={rightPanelExpanded}
+                    onClick={toggleRightPanel}
                   >
-                    Profile
-                  </SideNavLink>
-                  <SideNavMenu large renderIcon={Cognitive20} title="Experience">
-                    <SideNavMenuItem href={`#${Paths.SKILLS}`} isActive={isPathMatch(Paths.SKILLS, location.pathname)}>
-                      Technical skills
-                    </SideNavMenuItem>
-                    <SideNavMenuItem href={`#${Paths.EMPLOYMENT}`} isActive={isPathMatch(Paths.EMPLOYMENT, location.pathname)}>
-                      Employment
-                    </SideNavMenuItem>
-                    <SideNavMenuItem href={`#${Paths.PROJECTS}`} isActive={isPathMatch(Paths.PROJECTS, location.pathname)}>
-                      Projects
-                    </SideNavMenuItem>
-                    <SideNavMenuItem href={`#${Paths.EDUCATION}`} isActive={isPathMatch(Paths.EDUCATION, location.pathname)}>
-                      Education
-                    </SideNavMenuItem>
-                  </SideNavMenu>
-                  <SideNavMenu large renderIcon={Document20} title="Documents">
-                    <SideNavMenuItem href={`#${Paths.RESUME}`} isActive={isPathMatch(Paths.RESUME, location.pathname)}>
-                      Resume
-                    </SideNavMenuItem>
-                    <SideNavMenuItem href={`#${Paths.CV}`} isActive={isPathMatch(Paths.CV, location.pathname)}>
-                      Curriculum vitae
-                    </SideNavMenuItem>
-                  </SideNavMenu>
-                  <SideNavLink
-                    large
-                    renderIcon={Chat20}
-                    href={`#${Paths.CONTACT}`}
-                    isActive={isPathMatch(Paths.CONTACT, location.pathname)}
-                  >
-                    Contact
-                  </SideNavLink>
-                </SideNavItems>
-              </SideNav>
-              <HeaderPanel aria-label="Header panel" expanded={rightPanelExpanded}>
-                <Switcher aria-label="Switcher container">
-                  <SwitcherItem aria-label="Linkedin" target="_blank" href={Links.LINKEDIN}>
-                    <LogoLinkedin24 /> Linkedin
-                  </SwitcherItem>
-                  <SwitcherItem aria-label="Github" target="_blank" href={Links.GITHUB}>
-                    <LogoGithub24 /> Github
-                  </SwitcherItem>
-                  <SwitcherItem aria-label="Facebook" target="_blank" href={Links.FACEBOOK}>
-                    <LogoFacebook24 /> Facebook
-                  </SwitcherItem>
-                  <SwitcherItem aria-label="Instagram" target="_blank" href={Links.INSTAGRAM}>
-                    <LogoInstagram24 /> Instagram
-                  </SwitcherItem>
-                  <SwitcherItem aria-label="Twitter" target="_blank" href={Links.TWITTER}>
-                    <LogoTwitter24 /> Twitter
-                  </SwitcherItem>
-                </Switcher>
-              </HeaderPanel>
-            </Header>
-            <Content id="main-content">
-              <Routes>
-                <Route path={Paths.HOME} element={<HomePage />} exact />
-                <Route path={Paths.SKILLS} element={<SkillsPage />} />
-                <Route path={Paths.EMPLOYMENT} element={<EmploymentPage />} />
-                <Route path={Paths.PROJECTS} element={<ProjectsPage />} />
-                <Route path={Paths.EDUCATION} element={<EducationPage />} />
-                <Route path={Paths.RESUME} element={<ResumePage />} />
-                <Route path={Paths.CV} element={<CvPage />} />
-                <Route path={Paths.CONTACT} element={<ContactsPage />} />
-                <Route path="*" element={<Navigate replace to={Paths.HOME} />} />
-              </Routes>
-            </Content>
-          </>
-        )}
+                    <AppSwitcher20 />
+                  </HeaderGlobalAction>
+                </HeaderGlobalBar>
+                <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
+                  <SideNavItems>
+                    <SideNavLink
+                      large
+                      renderIcon={UserAvatar20}
+                      href={`#${Paths.HOME}`}
+                      isActive={isPathMatch(Paths.HOME, location.pathname)}
+                    >
+                      Profile
+                    </SideNavLink>
+                    <SideNavMenu large renderIcon={Cognitive20} title="Experience">
+                      <SideNavMenuItem href={`#${Paths.SKILLS}`} isActive={isPathMatch(Paths.SKILLS, location.pathname)}>
+                        Technical skills
+                      </SideNavMenuItem>
+                      <SideNavMenuItem href={`#${Paths.EMPLOYMENT}`} isActive={isPathMatch(Paths.EMPLOYMENT, location.pathname)}>
+                        Employment
+                      </SideNavMenuItem>
+                      <SideNavMenuItem href={`#${Paths.PROJECTS}`} isActive={isPathMatch(Paths.PROJECTS, location.pathname)}>
+                        Projects
+                      </SideNavMenuItem>
+                      <SideNavMenuItem href={`#${Paths.EDUCATION}`} isActive={isPathMatch(Paths.EDUCATION, location.pathname)}>
+                        Education
+                      </SideNavMenuItem>
+                    </SideNavMenu>
+                    <SideNavMenu large renderIcon={Document20} title="Documents">
+                      <SideNavMenuItem href={`#${Paths.RESUME}`} isActive={isPathMatch(Paths.RESUME, location.pathname)}>
+                        Resume
+                      </SideNavMenuItem>
+                      <SideNavMenuItem href={`#${Paths.CV}`} isActive={isPathMatch(Paths.CV, location.pathname)}>
+                        Curriculum vitae
+                      </SideNavMenuItem>
+                    </SideNavMenu>
+                    <SideNavLink
+                      large
+                      renderIcon={Chat20}
+                      href={`#${Paths.CONTACT}`}
+                      isActive={isPathMatch(Paths.CONTACT, location.pathname)}
+                    >
+                      Contact
+                    </SideNavLink>
+                  </SideNavItems>
+                </SideNav>
+                <HeaderPanel aria-label="Header panel" expanded={rightPanelExpanded}>
+                  <Switcher aria-label="Switcher container">
+                    <SwitcherItem aria-label="Linkedin" target="_blank" href={Links.LINKEDIN}>
+                      <LogoLinkedin24 /> Linkedin
+                    </SwitcherItem>
+                    <SwitcherItem aria-label="Github" target="_blank" href={Links.GITHUB}>
+                      <LogoGithub24 /> Github
+                    </SwitcherItem>
+                    <SwitcherItem aria-label="Facebook" target="_blank" href={Links.FACEBOOK}>
+                      <LogoFacebook24 /> Facebook
+                    </SwitcherItem>
+                    <SwitcherItem aria-label="Instagram" target="_blank" href={Links.INSTAGRAM}>
+                      <LogoInstagram24 /> Instagram
+                    </SwitcherItem>
+                    <SwitcherItem aria-label="Twitter" target="_blank" href={Links.TWITTER}>
+                      <LogoTwitter24 /> Twitter
+                    </SwitcherItem>
+                  </Switcher>
+                </HeaderPanel>
+              </Header>
+              <Content id="main-content">
+                <Routes>
+                  <Route path={Paths.HOME} element={<HomePage />} exact />
+                  <Route path={Paths.SKILLS} element={<SkillsPage />} />
+                  <Route path={Paths.EMPLOYMENT} element={<EmploymentPage />} />
+                  <Route path={Paths.PROJECTS} element={<ProjectsPage />} />
+                  <Route path={Paths.EDUCATION} element={<EducationPage />} />
+                  <Route path={Paths.RESUME} element={<ResumePage />} />
+                  <Route path={Paths.CV} element={<CvPage />} />
+                  <Route path={Paths.CONTACT} element={<ContactsPage />} />
+                  <Route path="*" element={<Navigate replace to={Paths.HOME} />} />
+                </Routes>
+              </Content>
+            </>
+          );
+        }}
       />
     </div>
   );
