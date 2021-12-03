d0b7df15553cc47be0eab213ecb5ff22f4dc4382
jacob-alspaw <jacob.alspaw@hcl.com>
2021-12-03 14:26:25 -0700

    Add text and basic styles to profile page

--- src/modules/home/HomePage.jsx
+++ src/modules/home/HomePage.jsx
@@ -6,13 +6,29 @@ import './HomePage.scss';
 const bem = defineBlock('HomePage');
 
 const HomePage = () => (
-  <PageBase title="Profile" wip>
+  <PageBase
+    title="Profile"
+    subtitle="Here's what you should know about me..."
+  >
     <div className={bem()}>
       <img
         className={bem('profile-image')}
-        src="images/profile-circle--small.png"
+        src="images/profile.jpg"
         alt="profile"
       />
+      <div className={bem('info')}>
+        <div className={bem('name')}>Jacob Alspaw</div>
+        <div className={bem('jobs')}>
+          <div className={bem('title')}>UI/UX LEAD DEVELOPER</div>
+          <div className={bem('title')}>FULL-STACK SOFTWARE DEVELOPER</div>
+        </div>
+        <div className={bem('statement')}>
+          This is me. I am a software developer. I like to code and I do it fast.
+          This is me. I am a software developer. I like to code and I do it fast.
+          This is me. I am a software developer. I like to code and I do it fast.
+          This is me. I am a software developer. I like to code and I do it fast.
+        </div>
+      </div>
     </div>
   </PageBase>
 );
