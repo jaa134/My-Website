24e7123d8f0249eb6cdf052046ea2f7bd21d488d
jacob-alspaw <jacob.alspaw@hcl.com>
2021-12-06 12:03:45 -0700

    Add photo album

--- src/modules/home/HomePage.jsx
+++ src/modules/home/HomePage.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 import defineBlock from '../../utils/defineBlock';
 import PageBase from '../../common/PageBase';
-import PhotoAlbum from './PhotoAlbum';
+import { photos } from './constants';
 import './HomePage.scss';
 
 const bem = defineBlock('HomePage');
 
-const HomePage = () => (
-  <PageBase
-    title="Profile"
-    subtitle="Here's what you should know about me"
-  >
-    <div className={bem()}>
-      <div className={bem('about')}>
-        <img
-          className={bem('profile-image')}
-          src="images/profile.jpg"
-          alt="profile"
-        />
+const HomePage = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const openLightbox = useCallback((event, { index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+  return (
+    <PageBase
+      title="Profile"
+      subtitle="Here's what you should know about me"
+    >
+      <div className={bem()}>
         <div className={bem('info')}>
           <div className={bem('name')}>Jacob Alspaw</div>
           <div className={bem('jobs')}>
@@ -32,10 +39,24 @@ const HomePage = () => (
             This is me. I am a software developer. I like to code and I do it fast.
           </div>
         </div>
+        <Gallery photos={photos} onClick={openLightbox} />;
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map((x) => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </div>
-      <PhotoAlbum />
-    </div>
-  </PageBase>
-);
+    </PageBase>
+  );
+};
 
 export default HomePage;
--- src/modules/home/constants.js
+++ src/modules/home/constants.js
@@ -1,3 +1,131 @@
-export const Photos = [
-  'images/profile.jpg'
-]
+export const albumPath = 'images/album';
+
+export const photos = [{
+  src: `${albumPath}/7m2FbjvtBR.jpg`,
+  width: 2247,
+  height: 3024
+}, {
+  src: `${albumPath}/DQRFjLD1r9.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/UE80JN27CO.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/xhEZaOycWJ.jpg`,
+  width: 6000,
+  height: 4000
+}, {
+  src: `${albumPath}/O5WQ7PXP7T.jpg`,
+  width: 3354,
+  height: 4494
+}, {
+  src: `${albumPath}/rWNp9zLvg3.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/5TdbynmrL3.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/Z2mBwCzLCT.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/QyLkeh5QXH.jpg`,
+  width: 3310,
+  height: 2364
+}, {
+  src: `${albumPath}/ZTronsx4W9.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/m2zOmedogZ.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/gHIfkZqH7P.jpg`,
+  width: 3088,
+  height: 2316
+}, {
+  src: `${albumPath}/xZZAh1PkhZ.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/nHu2voM4WI.jpg`,
+  width: 2048,
+  height: 1536
+}, {
+  src: `${albumPath}/BeitVckLCP.jpg`,
+  width: 2250,
+  height: 3000
+}, {
+  src: `${albumPath}/rhSI0TIP7Q.jpg`,
+  width: 3000,
+  height: 2000
+}, {
+  src: `${albumPath}/kW1CYFV8ra.jpg`,
+  width: 3000,
+  height: 2000
+}, {
+  src: `${albumPath}/I593Xr7sMZ.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/MksrB66p6Q.jpg`,
+  width: 2268,
+  height: 3024
+}, {
+  src: `${albumPath}/a8w4c2lSCT.jpg`,
+  width: 1440,
+  height: 1800
+}, {
+  src: `${albumPath}/ONhrIyerW8.jpg`,
+  width: 1440,
+  height: 1800
+}, {
+  src: `${albumPath}/oajwCQU6M0.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/VN3ISXANh2.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/eHTLdxiah1.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/cBkzPutqWH.jpg`,
+  width: 2316,
+  height: 3088
+}, {
+  src: `${albumPath}/XpCUgwT36R.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/38JhxDw2h0.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/KQDWtaYRM2.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/Ie4gABa8rJ.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/EZMnWVbb7S.jpg`,
+  width: 3024,
+  height: 4032
+}, {
+  src: `${albumPath}/9tBRzSjIWa.jpg`,
+  width: 4032,
+  height: 3024
+}, {
+  src: `${albumPath}/1EUNUbourI.jpg`,
+  width: 4032,
+  height: 3024
+}];
